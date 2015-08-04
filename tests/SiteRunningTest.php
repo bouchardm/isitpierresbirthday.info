808bde0aa98df0e2fd9551dd7ef4f59b36766fce
Marc-Antoine Bouchard Marceau <marcantoine.bouchardm@gmail.com>
2015-08-03 22:41:05 -0400

    Modif gitignore

--- tests/SiteRunningTest.php
+++ tests/SiteRunningTest.php
@@ -4,16 +4,16 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class SiteRunningTest extends TestCase
 {
     /**
      * A basic functional test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    public function testIsTheSiteRunning()
     {
         $this->visit('/')
-             ->see('Laravel 5');
+             ->see('YES');
     }
 }
